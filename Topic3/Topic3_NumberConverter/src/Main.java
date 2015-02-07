ea38c6141c8ed3440949386be5991be64452d358
Pedro Russo <wildestier01@gmail.com>
2015-02-07 20:06:48 -0300

    Revert "Revert "Revert "topic -5 (unfinished)"""
    This reverts commit e20a16508d5db6b828030c2a9bad7a9840be630a.

--- Topic3/Topic3_NumberConverter/src/Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		RomanNumber roman = new RomanNumber(2408);
-		System.out.println(roman.convert(roman.getNumber()));
-	}
-
-}
